a2ced92d962a794912f81732f5f068728b4d8f39
Sri Dharani R <sridharanirajendran26@gmail.com>
2024-05-18 18:35:19 +0530

    added verted

--- first.cpp
+++ first.cpp
@@ -1,5 +1,58 @@
 #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main(){
-    std::cout<<"Hello "<<std::endl;
+#define INF 999
+
+struct Card {
+    int value;
+    char symbol;
+};
+
+class vertex {
+public:
+    Card card; 
+    vector<pair<vertex*, int>> wadj; 
+    bool visited;
+
+    vertex(int s, char c) : card{ s, c }, visited(false) {}
+};
+
+class Graph {
+public:
+    int n;
+    vector<vertex*> graph;
+
+    Graph() : n(0) {}
+
+    ~Graph() {
+        for (auto v : graph) {
+            delete v;
+        }
+    }
+
+    void ivertex(Card c) {
+        vertex* v = new vertex(c.value, c.symbol);
+        graph.push_back(v);
+        n++;
+    }
+
+    void printvertex(){
+        for(auto i:graph){
+            cout<<i->card.symbol<<" "<<i->card.value<<endl;
+        }
+    }
+};
+
+int main() {
+    Graph g;
+
+    g.ivertex({1,'a'});
+    g.ivertex({1,'a'});
+    g.ivertex({1,'a'});
+
+    g.printvertex();
+
+
+    return 0;
 }
