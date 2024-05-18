ac8775e9d176966e88889c07dabb55b55e53052a
Sri Dharani R <sridharanirajendran26@gmail.com>
2024-05-18 18:50:33 +0530

    addededgetoo

--- first.cpp
+++ first.cpp
@@ -42,16 +42,53 @@ public:
             cout<<i->card.symbol<<" "<<i->card.value<<endl;
         }
     }
+
+    void addedge(){
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    if(graph[i]->card.symbol==graph[j]->card.symbol){
+                        graph[i]->wadj.push_back(make_pair(graph[j],abs(graph[i]->card.value-graph[j]->card.value)));
+                    }
+                    if(graph[i]->card.value==graph[j]->card.value){
+                        graph[i]->wadj.push_back(make_pair(graph[j],abs(graph[i]->card.value-graph[j]->card.value)));
+                    }
+                }
+            }
+        }
+    }
+
+
+    void printadj(){
+        for(auto i:graph){
+            cout<<i->card.symbol<<i->card.value<<"->";
+            for(auto j:i->wadj){
+                cout<<j.first->card.symbol<<j.first->card.value<<" "<<j.second<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+
+   
+
+
 };
+
 
 int main() {
     Graph g;
 
     g.ivertex({1,'a'});
-    g.ivertex({1,'a'});
-    g.ivertex({1,'a'});
+    g.ivertex({2,'a'});
+    g.ivertex({3,'a'});
+    g.ivertex({3,'b'});
 
-    g.printvertex();
+    g.ivertex({4,'h'});
+    // g.printvertex();
+    g.addedge();
+    g.printadj();
 
 
     return 0;
